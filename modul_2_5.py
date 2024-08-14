295dad3789b7c7c37c6204c5774f27de9b4bdf4b
Demyan k. <demiandiaz.dd@gmail.com>
2024-08-14 20:53:31 +0300

    Add files via upload

---
+++ modul_2_5.py
@@ -0,0 +1,23 @@
+def get_matrix(n, m, value):
+
+    matrix = []
+
+    # внешний цикл для строк
+    for i in range(n):
+        list_ = []
+
+        # Внутренний цикл для столбцов
+        for j in range(m):
+            list_.append(value)
+
+        matrix.append(list_)
+
+    return matrix
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+
+print(result1)
+print(result2)
+print(result3)
